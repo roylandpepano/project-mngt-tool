fce7a64312d76667fc7b0a64454ad9ff553ab4af
Royland Pepaño <58173787+roylandpepano@users.noreply.github.com>
2025-09-11 00:04:58 +0800

    - added a redirect middleware to stop authenticated users from viewing guest pages like welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\RedirectIfAuthenticated;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(RedirectIfAuthenticated::class)->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
